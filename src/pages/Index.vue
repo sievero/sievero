b671903cac72350edcf7982264a57e91db6de6e2
Cavanaugh Richards <cavatron@pm.me>
2020-01-01 11:06:30 -0600

    add big text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,11 @@
 <template>
-  <q-page padding> </q-page>
+  <q-page padding class="bg-blue-grey-1">
+    <div class="row justify-center q-mt-md">
+      <div class="col-md-8">
+        <div class="text-h3 text-center">Passive job search for everyone</div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
